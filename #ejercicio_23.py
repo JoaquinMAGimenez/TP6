1c045468027b88eedbc19a1bf585a10902e0cd01
JoaquinMAGimenez <101671247+JoaquinMAGimenez@users.noreply.github.com>
2022-11-14 13:02:05 -0300

    Add files via upload

--- #ejercicio_23.py
+++ #ejercicio_23.py
@@ -58,7 +58,8 @@ print("LISTADO DE LAS CRIATURAS Y QUIENES LAS DERROTARON:")
 modulo_arbol.inorden_criaturas_derrotadas(arbol_de_criaturas)
 
 #B
-
+print('                                                           ')
+modulo_arbol.setDescripcion(arbol_de_criaturas)
 #C
 print('                                                           ')
 nodo_buscado = modulo_arbol.busqueda(arbol_de_criaturas, "Talos")
@@ -66,6 +67,29 @@ print("Informacion de Talos: ", nodo_buscado)
 
 #D
 print('                                                           ')
+vec_heroes = modulo_arbol.obtener_heroes(arbol_de_criaturas)   # Obtengo un vector con todos los heroes y 
+                                                            # estarán repetidos si derrotó a más de una criatura
+
+cantidades = collections.Counter(vec_heroes)  # Devuelve un diccionario con el nombre del heroe 
+                                              # y la cantidad de recurrencias en el vector
+print('                                                           ')
+print("HEROES QUE MAS CRIATURAS DERROTARON: ")
+for i in range(3): 
+    h = (max(cantidades, key=cantidades.get))       # Obtengo del diccionario el heroe que mas criaturas derroto
+    cantidad_h = cantidades[h]                      # El numero de criaturas que derrotó
+    print(h, "ha derrotado a", cantidad_h, "criaturas")
+    cantidades.pop(h)
+
+    continuar = True
+    while(continuar == True):   
+        if(len(cantidades) > 0):  
+            new_h =  max(cantidades, key=cantidades.get)       # Control de si hay más heroes con la misma cantidad de criaturas derrotadas
+            if (cantidades[new_h] == cantidad_h):
+                print(new_h, "tambien derrotó a", cantidades[new_h], "criaturas.")
+                cantidades.pop(new_h)
+            else:
+                continuar = False
+        else: continuar = False
 
 #E
 print('                                                           ')
@@ -80,16 +104,28 @@ print('                                                           ')
 
 #H
 print('                                                           ')
-
+buscado = modulo_arbol.busqueda(arbol_de_criaturas, 'Cerbero')
+buscado['datos']['capturado_por'] = 'Heracles'
+buscado = modulo_arbol.busqueda(arbol_de_criaturas, 'Toro de Creta')
+buscado['datos']['capturado_por'] = 'Heracles'
+buscado = modulo_arbol.busqueda(arbol_de_criaturas, 'Cierva de Cerina')
+buscado['datos']['capturado_por'] = 'Heracles'
+buscado = modulo_arbol.busqueda(arbol_de_criaturas, 'Jabalí de Erimanto')
+buscado['datos']['capturado_por'] = 'Heracles'
 #I
 print('                                                           ')
-
+while(opcion == 'n'):
+    clave = input("Ingrese parte de lo que desea buscar: (Mayúscula al inicio): ")
+    buscado = modulo_arbol.busqueda_por_proximidad(arbol_de_criaturas, clave)
+    print(buscado['info'])
+    opcion = input("Es esta la criatura buscada? s/n: ")
 #J
 modulo_arbol.eliminar_nodo(arbol_de_criaturas, 'Basilisco')
 modulo_arbol.eliminar_nodo(arbol_de_criaturas, 'Sirenas')
 #k
 print('                                                           ')
-
+nodo_aves = modulo_arbol.busqueda(arbol_de_criaturas, 'Aves del Estínfalo')
+nodo_aves['datos']['derrotado_por'] = 'Heracles ha derroatado a varias'
 #L
 print('                                                           ')
 nodo_ladon = modulo_arbol.busqueda(arbol_de_criaturas, 'Ladón')
